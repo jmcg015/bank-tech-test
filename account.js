bfa81abb6470195a68d8ab650bf8ccb90630130c
Joe McGarry <joewmcgarry@gmail.com>
2022-07-23 13:05:00 +0100

    Adds deposit() with passing tests

--- account.js
+++ account.js
@@ -3,8 +3,13 @@ class Account {
   constructor() {
     this.balance = 0.00;
   }
+
   showBalance() {
     return this.balance;
+  }
+
+  deposit(amount) {
+    return this.balance += amount;
   }
 }
 
--- account.test.js
+++ account.test.js
@@ -1,10 +1,18 @@
 const Account = require('./account');
 
 describe('Account', () => {
-  describe('Initially', () => {
-    it('has a balance of 0.00', () => {
+  describe('showBalance', () => {
+    it('initially has a balance of 0.00', () => {
       const account = new Account();
       expect(account.showBalance()).toEqual(0.00);
+    })
+  })
+
+  describe('Deposit', () => {
+    it("adds 5.00 to the balance", () => {
+      const account = new Account();
+      account.deposit(5.00);
+      expect(account.showBalance()).toEqual(5.00);
     })
   })
 
