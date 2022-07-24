c2c79fb597ad849538c8e114f0a6cf9bea9418bb
Joe McGarry <joewmcgarry@gmail.com>
2022-07-24 16:43:11 +0100

    Refactors transaction's date field

--- transaction.js
+++ transaction.js
@@ -1,4 +1,8 @@
 class Transaction {
+  constructor() {
+    this.transactionDate = `${new Date().toLocaleDateString("en-GB")} `;
+  }
+
   showTransaction() {
     return "24/07/2022 || || || "
   }
--- transaction.test.js
+++ transaction.test.js
@@ -3,6 +3,7 @@ const Transaction = require('./transaction')
 describe("Transaction", () => {
   it("displays the date", () => {
     const transaction = new Transaction();
-    expect(transaction.showTransaction()).toEqual("24/07/2022 || || || ");
+    const mockedDate = "24/07/2022"
+    expect(transaction.showTransaction()).toEqual(`${mockedDate} || || || `);
   })
 })
